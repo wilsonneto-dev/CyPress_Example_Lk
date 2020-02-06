98fdc9e78d65050e360d905d06b599d2de927bc3
Wilson Neto Dev <contato@wilsonneto.com.br>
2020-02-06 10:38:18 -0300

    updating...

--- cypress/integration/Forms/Login.spec.js
+++ cypress/integration/Forms/Login.spec.js
@@ -10,6 +10,20 @@ describe('Is site online and navigable?', () => {
       .its('.lightbox')
       .then($lightbox => {
         cy.get('.lightbox').click(150, 150);
+        cy.get(
+          ':nth-child(6) > .frame-center > .mediaCarousel > .frame-container > .frame > .items > [mediaid="149392"] > .mediaContainer > a > .mediaCover > .media-image > img'
+        ).click();
+        cy.contains('Ver mais detalhes').click();
+        cy.url().should('contain', 'filme');
+        cy.go('back');
+      });
+  });
+
+  it('Fechar a lightbox', () => {
+    cy.get('body')
+      .its('.lightbox')
+      .then($lightbox => {
+        cy.get('.lightbox').click(150, 150);
       });
   });
 });
